cd4fdfcc5b8bfb74ddec0a76257aa579606abd82
grindfever <miguelbravofigueiredo@gmail.com>
2023-12-07 17:09:15 +0000

    added viewports instead of fixed measures

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -3,11 +3,12 @@
 @section('title', 'Random')
 
 @section('content')
+    <div class="postpage">
     <section id="post">
         @include('partials.post', ['post' => $post])
     </section>
     @auth
     <button class="edit-post" data-post-id="{{ $post->id }}" type="submit">Edit</button>
     @endauth
-    
+    </div>
 @endsection
