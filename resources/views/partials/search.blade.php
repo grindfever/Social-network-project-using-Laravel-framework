5e8433460b2f42b3652bad14bdbc0c50da3b9953
Diogo Leandro <up202005304@up.pt>
2023-12-14 11:17:31 +0000

    Style for search bar

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,11 +1,13 @@
 <section id="search-section">
     <article class="search-article">
-        <form class="d-flex" action="/dashboard" method="POST">
-            @csrf
-            <input class="form-control me-sm-2" type="text" name="query" placeholder="search">
-            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        </form>
-        <button class="close-button" onclick="clearSearchResults()">x</button>
+        <div class="search-container">
+            <form class="d-flex" action="/dashboard" method="POST">
+                @csrf
+                <input class="form-control me-sm-2" type="text" name="query" placeholder="search">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            <button class="close-button" onclick="clearSearchResults()">x</button>
+        </div>
         <div class="search-results-container">
             <div class="search-results"></div>
         </div>
