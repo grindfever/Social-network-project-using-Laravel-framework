ae705a422a75a6f07a0351a55ec1ada7f6cdf31e
grindfever <miguelbravofigueiredo@gmail.com>
2023-12-21 20:59:40 +0000

    fixed css on friendlist

--- resources/views/partials/friend.blade.php
+++ resources/views/partials/friend.blade.php
@@ -1,13 +1,13 @@
 <article class="friend" data-id="{{ $friend->id }}">
-    <div class="profile-picture-name">
+
     <div class="profile-picture">
         <img src="{{ $friend->getProfileImage()}}" class="avatar" alt="Friend Picture">
     </div>
 
-    <div class="name redirecting to profile">
+    <div class="name-redirect">
             <a href="/profile/{{$friend->id}}" >{{$friend->name}}</a>
     </div>
-</div>
+
     <div class="removefriend">
         <button class="btn btn-dark" id="delete-friend" data-friend-id="{{$friend->id}}" type="submit">Remove Friend</button>
     </div>
